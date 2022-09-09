96edf718229600fe6be777e5b5f296dffeb82c84
Gig2341 <danmata2341@gmail.com>
2022-09-09 11:36:01 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char word;
+	for (word = 'a'; word <= 'z'; word++)
+		putchar(word);
+	for (word = 'A'; word <= 'Z'; word++)
+		putchar(word);
+	putchar('\n');
+	return (0);
+}
