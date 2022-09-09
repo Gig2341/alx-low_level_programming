418073597a89432c7e9351a15ec2a9f04de9a4cd
Gig2341 <danmata2341@gmail.com>
2022-09-09 14:06:39 -0700

    final additional

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,8 +12,8 @@ int main(void)
 {
 	char word;
 
-	for (word = 'a'; word <= 'z'; word++)
-			putchar(word);
+	for (word = 'a'; word <= 'z'; word++)	
+		putchar(word);
 	putchar('\n');
 	return (0);
 }
