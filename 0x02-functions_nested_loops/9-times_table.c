3ad3f9319a92885ceb1949d5f4fb625e8545247c
Gig2341 <danmata2341@gmail.com>
2022-09-13 17:16:25 -0700

    final 11

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,53 +1,41 @@
 #include "main.h"
 
-
-
 /**
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
  *
- *  * jack_bauer - prints time
- *
- *   *
- *
- *    * Description: prints a list of time
- *
- *     * Return: Always 0.
- *
- *      */
+ * Return: times table
+ * add extra space past single digit
+ */
 
-
-
-void jack_bauer(void)
-
+void times_table(void)
 {
+	int r, c, d;
 
-		int i, j;
-
-
-
-			for (i = 0; i < 24; i++)
-
-					{
-
-								for (j = 0; j < 60; j++)
-
-											{
-
-															_putchar(i / 10 + 48);
-
-																		_putchar(i % 10 + 48);
-
-																					_putchar(':');
-
-																								_putchar(j / 10 + 48);
-
-																											_putchar(j % 10 + 48);
-
-																														_putchar('\n');
-
-																																}
-
-									}
-
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+												for (c = 1; c <= 9; c++)
+		{
+			d = (r * c);
+		if ((d / 10) > 0)
+		{
+			_putchar((d / 10) + '0');
+		}
+		else
+		{
+			_putchar(' ');
+		}
+			_putchar((d % 10) + '0');
+		if (c < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+											}
+		_putchar('\n');
+	}
 }
-
 
