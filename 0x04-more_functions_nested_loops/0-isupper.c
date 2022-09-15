2fa391af9c36868c63cf4f2a704dd5683eb39a68
Gig2341 <danmata2341@gmail.com>
2022-09-15 04:43:50 -0700

    0-upper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * tis function will check for for uppercase text
+ *
+ * Returns 1 if c is uppercase
+ *
+ * Returns 0 otherwise
+ */
+
+int _isupper(int c);
+{
+	return (c >= 65 && c <= 90);
+}
