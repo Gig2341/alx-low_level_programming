4bcf04b2c789d5f7a98c3561fe0f8a98df225c0b
Gig2341 <danmata2341@gmail.com>
2022-09-15 04:51:08 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mian: checks whether c is a digit
+ *
+ * Returns 1 if c is a digit
+ *
+ * Returns 0 otherwise
+ */
+
+int _isdigit(int c);
+{
+	return (c >= 48 && c <= 57);
+}
