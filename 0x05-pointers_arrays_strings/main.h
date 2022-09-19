2c803101b294d2a1dd520f313630b33f89248cc0
Gig2341 <55427509+Gig2341@users.noreply.github.com>
2022-09-19 20:05:10 +0000

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char);
+int _putchar(char c);
 void reset_to_98(int *n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -15,4 +15,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* MAIN-H */
+#endif /* MAIN_H */
