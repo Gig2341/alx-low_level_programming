99e79f9dee664391cae34ea70a96a27f2a99c2fa
Gig2341 <danmata2341@gmail.com>
2022-09-19 15:56:00 -0700

    corrections

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,24 +8,55 @@
 
 int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int total = 0;
-	char null_flag = 0;
 
-	while (*s)
-	{
-		if (*s == '-')
-			sign *= -1;
-		if (*s >= '0' && *s <= '9')
-		{
-			null_flag = 1;
-			total = total * 10 + *s - '0';
-		}
-		else if (null_flag)
-			break;
-			s++;
-	}
-	if (sign < 0)
-		total = (-total);
-	return (total);
+		unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+
+
+			while (*(s + count) != '\0')
+
+					{
+
+								if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+
+												break;
+
+
+
+										if (*(s + count) == '-')
+
+														pn *= -1;
+
+
+
+												if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+
+															{
+
+																			if (size > 0)
+
+																								m *= 10;
+
+																						size++;
+
+																								}
+
+														count++;
+
+															}
+
+
+
+				for (i = count - size; i < count; i++)
+
+						{
+
+									oi = oi + ((*(s + i) - 48) * m);
+
+											m /= 10;
+
+												}
+
+					return (oi * pn);
+
 }
