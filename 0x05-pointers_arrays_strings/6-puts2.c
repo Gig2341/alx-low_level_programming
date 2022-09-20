7489b384c2b06a32ffe3dd8f19ed60d0dcb4d529
Gig2341 <danmata2341@gmail.com>
2022-09-19 18:18:47 -0700

    corrections

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,53 +1,24 @@
 #include "main.h"
 
 /**
- * puts2 - a function that prints every other character of a string,
- * starting with the first character, followed by a new line.
- * @str: An input string
- * Return: Nothing
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
  */
 
 void puts2(char *str)
 {
+	int count = 0;
 
-		int count = 0, i, j;
-
-			char *str, temp;
-
-
-
-				while (count >= 0)
-
-						{
-
-									if (s[count] == '\0')
-
-													break;
-
-											count++;
-
-												}
-
-					str = s;
-
-
-
-						for (i = 0; i < (count - 1); i++)
-
-								{
-
-											for (j = i + 1; j > 0; j--)
-
-														{
-
-																		temp = *(str + j);
-
-																					*(str + j) = *(str + (j - 1));
-
-																								*(str + (j - 1)) = temp;
-
-																										}
-
-												}
-
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+				break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+			count++;
+	}
 }
