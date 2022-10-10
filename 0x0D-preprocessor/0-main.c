d40692e94afa8d8df417e8773a47ae22236fd088
Gig2341 <danmata2341@gmai.com>
2022-10-10 08:57:17 -0700

    task 2

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -1,33 +1,18 @@
 #include "0-object_like_macro.h"
-
 #include "0-object_like_macro.h"
-
 #include <stdio.h>
 
-
-
 /**
+ * main - check the code
  *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+ * Return: Always 0.
+ */
 
 int main(void)
-
 {
-
-	    int s;
-
-
-
-	        s = 98 + SIZE;
-
-		    printf("%d\n", s);
-
-		        return (0);
-
+    int s;
+    
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
 }
