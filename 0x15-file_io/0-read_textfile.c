6614fd75b18790bd8ecc4d91e14c9afe83f60d4f
Gig2341 <danmata2341@gmail.com>
2022-10-31 14:16:11 +0000

    task 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * read_textfile - a function that reads a text file
+ * and prints it to the POSIX standard output.
+ *
+ * main: returns actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
