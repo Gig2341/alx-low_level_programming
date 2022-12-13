279f2fab8a7c08cf65300cabd0c8a919324f0f9d
Gig2341 <danmata2341@gmail.com>
2022-12-13 09:44:33 +0000

    task 2

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * operation : perform mathematical operation
+ *
+ * main: return (0) 
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+int sub(int a,int b)
+{
+	return (a - b);
+}
+int mul(int a, int b)
+{
+	return (a * b);
+}
+int div(int a,int b)
+{
+	return (a / b);
+}
+int mod(int a,int b)
+{
+	return (a % b);
+}
