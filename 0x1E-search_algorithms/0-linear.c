af719b226163552dd78071d4650ffd9fe2eaa6e0
Gig2341 <danmata2341@gmail.com>
2023-04-12 05:31:17 +0000

    linear search done

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - performs a linear search on an array
+ *
+ * Args: array 
+ * 	size
+ * 	value
+ *
+ * return: Always 0
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array != NULL)
+	{
+		for ( i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%u]", i, array[i]);
+			if (value == array[i])
+			{
+				printf("Found %d at index: %lu", value , i);
+				return 0;
+			}
+		}
+	}
+	return (-1);
+}		
