ce6347dcf85d1c03d6e48b1f34efd419151b4912
Gig2341 <danmata2341@gmail.com>
2023-06-09 21:06:23 +0000

    hash tables created

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,16 @@
 
  hash_table_t *hash_table_create(unsigned long int size)
  {
-	new_hash_table = malloc(sizeof(hash_table_t))
+	unsigned long int i;
+	hash_table_t *new_hash_table;
+
+	new_hash_table = malloc(sizeof(hash_table_t));
 
 	if (new_hash_table == NULL)
 		return NULL;
 	
 	new_hash_table->size = size;
-	new_hash_table->array = malloc(sizeof(hash_node_t) * size)
+	new_hash_table->array = malloc(sizeof(hash_node_t) * size);
 
 	if (new_hash_table -> array == NULL)
 		return NULL;
